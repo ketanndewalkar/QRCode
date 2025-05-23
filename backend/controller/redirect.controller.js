d6fc2ef9ec93cedca81d3253010aed0dc5784d24
Ketan <dewalkarketan123@gmail.com>
2025-05-24 01:01:47 +0530

    Third commit

--- backend/controller/redirect.controller.js
+++ backend/controller/redirect.controller.js
@@ -2,12 +2,14 @@ const { Count } = require("../model/Brand");
 const { url, keywords } = require("../url");
 const redirectController = async (req, res) => {
   try {
-    const brand = Object.fromEntries(Object.entries(req.query)).brand;
+    const {brand} = req.query;
+    console.log(brand);
     const doc = await Count.find({ name:brand });
     if (!keywords.includes(brand) || doc.length==0){
         res.send("The url dont exist");
+        return ;
     }
-    Count.updateOne({ name: brand }, { $inc: { count: 1 } });
+    await Count.updateOne({ name: brand }, { $inc: { count: 1 } });
     res.redirect(url[keywords.indexOf(brand)]);
   } catch (error) {
     console.log(error);
